0263526caf8c2a4345c8882c4c5a49d030f1e477
jorge <jorgearojas25>
2018-08-17 08:59:27 -0500

    graficas y trabajo del determiante

---
+++ DeterminanteGaussJordan/js/script.js
@@ -0,0 +1,138 @@
+function generarMatriz() {
+	const n = parseInt(document.getElementById("entradaN").value);
+	const container = document.getElementById("container");
+	const divBotonSol = document.getElementById("botonSolucion");
+
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= n; j++) {
+			var input = document.createElement("input");
+			input.id = "input" + i + "-" + j;
+			container.insertBefore(input,divBotonSol);
+		}
+		var salto = document.createElement("br");
+		container.insertBefore(salto,divBotonSol);
+	}
+	document.getElementById("btnSol").style.display = "inline";
+}
+
+function llenarSolucionar(){
+	var cont = 0;
+	var sign = 1;
+	var det = 1;
+	const n = parseInt(document.getElementById("entradaN").value);
+	var matriz = new Array(n);
+	for(i=0; i<n; i++){
+		matriz[i] = new Array(n);
+	}
+
+	for(i=0; i<n; i++){
+		for(j=0; j<n; j++){
+			var input = "input" + (i+1) + "-" + (j+1);
+			console.log(input);
+			matriz[i][j] = parseInt(document.getElementById(input).value);
+			console.log(matriz[i][j]);
+		}
+	}
+
+	cont = 1;
+	for (i = 0; i < (n-1); i++){
+		cont = cont+6;
+		if(matriz[i][i]==0){
+			cont = cont +2;
+			for (h=i+1; h < n; h++) {
+				cont = cont+5;
+				if(matriz[h][i]!=0){
+					cont = cont+3;
+					sign = sign*(-1);
+					for (ch =0; ch<n; ch++) {
+						cont = cont+13;
+						var temp = matriz[h][ch];
+						matriz[h][ch] = matriz[i][ch];
+						matriz[i][ch] = temp;
+					}
+					h = n-1;
+					cont = cont+1;
+				}
+			}
+			cont = cont+1;
+		}
+		cont = cont+2;
+		for (k = (i + 1); k < n; k++) {
+			cont = cont+4;
+			for (j = (i + 1); j < n; j++) {
+				cont = cont+16;
+				matriz[k][j] = matriz[k][j] - (matriz[k][i] * matriz[i][j]) / matriz[i][i];
+			}
+		}
+	}
+	cont = cont+2;
+
+
+	for(i=0; i<n; i++){
+		det = det*matriz[i][i];
+	}
+
+	var a = (Math.pow(n,2)*(n-1))-(n*(n-2)*(n-1))-((2*n)*(n-1))+(((n-2)*(n-1)*((2*n)-3))/6)+((n-2)*(n-1))+(n-1);
+	var b = (n*(n-1))-(n-1)-(((n-2)*(n-1))/2);
+	var formula = (16*a)+(5*b)+(6*(n-2))+3;
+
+	det=det*sign;
+
+	mostrarEnPantalla(cont, matriz, det, formula);
+}
+
+function mostrarEnPantalla(cont, matrizReducida, det, form){
+	const container = document.getElementById("contenedorSol");
+	var matriz = document.createElement("div");
+	var contDiv = document.createElement("div");
+	var formDiv = document.createElement("div");
+	var detDiv = document.createElement("div");
+	var datos = "matriz reducida <br>";
+
+	matriz.style.display = "inline-block";
+	matriz.style.margin = "1em"
+	matriz.id = "divMatrizRedu";
+	contDiv.style.display = "inline-block";
+	contDiv.style.margin = "1em"
+	contDiv.id = "divCont"
+	formDiv.style.display = "inline-block";
+	formDiv.style.margin = "1em"
+	formDiv.id = "diForm"
+	detDiv.style.display = "inline-block";
+	detDiv.style.margin = "1em"
+	detDiv.id = "divDet"
+
+	/*for (i=0; i<matrizReducida.length; i++){
+		for (j=0; j<matrizReducida.length; j++){
+			datos = datos + " " + matrizReducida[i][j]
+			console.log((i+1) + "-" + (j+1));
+			console.log(matrizReducida[i][j]);
+			matriz.innerHTML = datos;
+		}
+		datos = datos + "<br>";
+	}*/
+
+	if(isNaN(det)){
+		det = "No existe el determinante";
+		contDiv.innerHTML = "<h5>Operaciones elementales por contador: </h5>" + cont;
+		formDiv.innerHTML = "<h5>Operaciones elementales por formula: </h5>" + form;
+		detDiv.innerHTML = "<h5>Determinante de la matriz: </h5>" + det;
+
+		container.appendChild(contDiv);
+		container.appendChild(formDiv);
+		container.appendChild(detDiv);
+
+	}else{
+		container.appendChild(matriz);
+		contDiv.innerHTML = "<h5>Operaciones elementales por contador: </h5>" + cont;
+		formDiv.innerHTML = "<h5>Operaciones elementales por formula: </h5>" + form;
+		detDiv.innerHTML = "<h5>Determinante de la matriz: </h5>" + det;
+
+		container.appendChild(contDiv);
+		container.appendChild(formDiv);
+		container.appendChild(detDiv);
+	}
+
+
+
+}
---
+++ MetodosdeOrdenamiento/js/casoPromedio.js
@@ -0,0 +1,145 @@
+var app = angular.module('OrdenamientoApp', ['nvd3']);
+app.controller('controller', function($scope) {
+
+  // Ordenamiento burbula simple
+  function bubbleSort(myArr){
+    var tamanio = myArr.length;
+    //var ecTempBPr = (5 + (7/2)( tamanio(tamanio - 1) - (tamanio / 2)(tamanio - 1)) + 6(tamanio - 1) +3;
+    // Contador para el número de operaciones del algoritmo burbuja
+    var contBubble = 1; // pass = 1;
+    for (var i = 0; i < tamanio-1; i++) {
+
+      
+      contBubble += 3; //i < tamanio-1; i++
+      contBubble += 2; // var j = i+1
+      for (var j = i+1; j < tamanio; j++) {
+        contBubble += 3; // j < tamanio; j++
+        contBubble += 3; // myArr[i] > myArr[j]
+        if (myArr[i] > myArr[j]) {
+          var temp = myArr[j];
+          myArr[j] = myArr[i];
+          myArr[i] = temp;
+          contBubble += 3.5; // var temp = myArr[j]; myArr[j] = myArr[i]; myArr[i] = temp;
+        }
+      }
+    }
+    return contBubble;
+  }
+  // Ordenamiento por selección
+  function selectionSort(myArr){
+    var size = myArr.length;
+    var contSelection = 1; // Contador para el número de operaciones del algorito de selección
+    for (var slot = 0; slot < size -1; slot ++) {
+      contSelection += 3;
+      var smallest = slot;
+      contSelection += 3;
+      for (var check = slot + 1; check < size; check++) {
+        contSelection += 5;
+        if (myArr[check] < myArr[smallest]) {
+          smallest = check;
+          contSelection++;
+        }
+      }
+      contSelection += 2;
+      if (smallest != slot) {
+        var tmpVal = myArr[smallest];
+        myArr[smallest] = myArr[slot];
+        myArr[slot] = tmpVal;
+        contSelection += 3.5;
+      }
+    }
+    contSelection ++;
+    return contSelection;
+  }
+
+  // Ordenamiento por inserción
+  function insertionSort(myArr) {
+    var len = myArr.length;
+    var contInsetion = 1; // Contador del número de operaciones
+    for (var i = 1; i < len; i++) {
+        var tmp = myArr[i];
+        contInsetion += 6;
+        for (var j = i - 1; j >= 0 && (myArr[j] > tmp); j--) {
+            myArr[j + 1] = myArr[j];
+            contInsetion += 8;
+        }
+        myArr[j + 1] = tmp;
+        contInsetion += 2;
+    }
+    contInsetion++;
+    return contInsetion;
+  }
+  // Sobreescribe arr
+  function generarArregloPromedio() {
+    $scope.data[0].values = [];
+    $scope.data[1].values = [];
+    $scope.data[2].values = [];
+    for (var i = 20; i < 201; i++) {
+      var arrB = [],
+      arrS = [],
+      arrI = [];
+      for (var j = 0; j < i; j++) {
+        var aleatorio = Math.floor((Math.random() * 200) + 1); // Genera un número aleatorio entre 1 y 100
+        arrB.push(aleatorio);
+        arrS.push(aleatorio);
+        arrI.push(aleatorio);
+      }
+      $scope.data[0].values.push({x: i, y: bubbleSort(arrB)});
+      $scope.data[1].values.push({x: i, y: selectionSort(arrS)});
+      $scope.data[2].values.push({x: i, y: insertionSort(arrI)});
+    }
+  }
+
+  // Muestra en el div correspondiente el arreglo ordenado
+  $scope.mostrarArreglo = function() {
+    generarArregloPromedio();
+  }
+
+  $scope.options = {
+    chart: {
+      type: 'lineChart',
+      height: 450,
+      margin : {
+        top: 20,
+        right: 20,
+        bottom: 40,
+        left: 90
+      },
+      x: function(d){ return d.x; },
+      y: function(d){ return d.y; },
+      useInteractiveGuideline: true,
+      xAxis: {
+        axisLabel: 'Tamaño Arreglo (X)'
+      },
+      yAxis: {
+        axisLabel: 'Nº Operaciones (Y)',
+        tickFormat: function(d){
+          return d3.format('.f')(d);
+        },
+        axisLabelDistance: 20
+      },
+    },
+    title: {
+      enable: true,
+      text: 'Tabla de comparación entre los algoritmos de ordenamiento'
+    }
+  };
+
+  $scope.data = [
+    {
+      key: 'Burbuja',
+      values: [],
+      color: '#00FF00'
+    },
+    {
+      key: 'Selección',
+      values: [],
+      color: '#FA58D0'
+    },
+    {
+      key: 'Inserción',
+      values: [],
+      color: '#2E2EFE'
+    }
+  ];
+});
